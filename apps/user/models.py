2909ba0c3f44548e1c424e88c3c4e9d2bd820c0e
Geet <geetkaushik09@gmail.com>
2024-02-23 13:56:24 +0530

    created sellerhome, models: kitchen, sales

--- apps/user/models.py
+++ apps/user/models.py
@@ -6,18 +6,16 @@ class Product(models.Model):
     description = models.TextField(null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/')
- 
     def __str__(self):
         return self.name
- 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
- 
+
     def __str__(self):
-        return f'{self.quantity} x {self.product.name}'
+        return f'{self.user} @ {self.quantity} x {self.product.name} - {self.date_added}'
     
 class contact_info(models.Model):
     u_email = models.EmailField()
